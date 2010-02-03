8d3bc52a98ed45abfb26d3167c1a11665665a899
unknown <mail.alex.lyman@gmail.com>
2010-02-03 04:19:38 -0700

    Added JsonSerializationContext

---
+++ source/Liquid.Json/JsonSerializationContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Liquid.Json {
+    public class JsonSerializationContext {
+        JsonSerializer serializer;
+        TextWriter writer;
+
+        internal JsonSerializationContext(JsonSerializer serializer, TextWriter writer) {
+            this.serializer = serializer;
+            this.writer = writer;
+        }
+
+        public void Write(char ch) {
+            writer.Write(ch);
+        }
+        public void Write(string str) {
+            writer.Write(str);
+        }
+
+        public IFormatProvider FormatProvider { get { return serializer.FormatProvider; } }
+
+        public void Serialize<T>(T value) {
+            serializer.Serialize<T>(value, this);
+        }
+        public void SerializeAs(Type type, object value) {
+            serializer.SerializeAs(type, value, this);
+        }
+    }
+}
