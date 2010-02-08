f48da9fecc03072b04095e2e7f6bbe89c92e0b1f
unknown <mail.alex.lyman@gmail.com>
2010-02-07 17:38:08 -0700

    Added Liquid.Json.Mvc project for integrating with ASP.NET MVC 2

---
+++ source/Liquid.Json.Mvc/ControllerExtension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Liquid.Json.Mvc {
+    /// <summary>
+    /// Provides extension methods for the System.Web.Mvc.Controller class
+    /// </summary>
+    public static class ControllerExtension {
+        /// <summary>
+        /// Creates a LiquidJsonActionResult for the specified object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="controller">The controller.</param>
+        /// <param name="object">The object.</param>
+        /// <returns>The LiquidJsonActionResult for the specified object</returns>
+        public static LiquidJsonActionResult<T> LiquidJson<T>(this Controller controller, T @object) {
+            return new LiquidJsonActionResult<T>(@object);
+        }
+
+        /// <summary>
+        /// Creates a LiquidJsonActionResult for the specified object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="controller">The controller.</param>
+        /// <param name="object">The object.</param>
+        /// <param name="behavior">The behavior.</param>
+        /// <returns>
+        /// The LiquidJsonActionResult for the specified object
+        /// </returns>
+        public static LiquidJsonActionResult<T> LiquidJson<T>(this Controller controller, T @object, JsonRequestBehavior behavior) {
+            return new LiquidJsonActionResult<T>(@object, behavior);
+        }
+        
+        /// <summary>
+        /// Creates a LiquidJsonActionResult for the specified object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="controller">The controller.</param>
+        /// <param name="object">The object.</param>
+        /// <param name="serializer">The serializer.</param>
+        /// <returns>
+        /// The LiquidJsonActionResult for the specified object
+        /// </returns>
+        public static LiquidJsonActionResult<T> LiquidJson<T>(this Controller controller, T @object, JsonSerializer serializer) {
+            return new LiquidJsonActionResult<T>(@object, serializer);
+        }
+
+        /// <summary>
+        /// Creates a LiquidJsonActionResult for the specified object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="controller">The controller.</param>
+        /// <param name="object">The object.</param>
+        /// <param name="behavior">The behavior.</param>
+        /// <param name="serializer">The serializer.</param>
+        /// <returns>
+        /// The LiquidJsonActionResult for the specified object
+        /// </returns>
+        public static LiquidJsonActionResult<T> LiquidJson<T>(this Controller controller, T @object, JsonRequestBehavior behavior, JsonSerializer serializer) {
+            return new LiquidJsonActionResult<T>(@object, behavior, serializer);
+        }
+    }
+}
---
+++ source/Liquid.Json.Mvc/LiquidJsonActionResult.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Liquid.Json.Mvc {
+    /// <summary>
+    /// An action that uses the Liquid.Json serialization API to serialize an object
+    /// </summary>
+    /// <typeparam name="T">The type of the result</typeparam>
+    public class LiquidJsonActionResult<T> : ActionResult {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LiquidJsonActionResult&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="object">The result object.</param>
+        public LiquidJsonActionResult(T @object)
+            : this(@object, JsonRequestBehavior.DenyGet, null) {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LiquidJsonActionResult&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="object">The result object.</param>
+        /// <param name="behavior">The behavior of the action.</param>
+        public LiquidJsonActionResult(T @object, JsonRequestBehavior behavior)
+            : this(@object, behavior, null) {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LiquidJsonActionResult&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="object">The result object.</param>
+        /// <param name="serializer">The serializer.</param>
+        public LiquidJsonActionResult(T @object, JsonSerializer serializer)
+            : this(@object, JsonRequestBehavior.DenyGet, serializer) {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LiquidJsonActionResult&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="object">The result object.</param>
+        /// <param name="behavior">The behavior of the action.</param>
+        /// <param name="serializer">The serializer.</param>
+        public LiquidJsonActionResult(T @object, JsonRequestBehavior behavior, JsonSerializer serializer) {
+            this.Object = @object;
+            this.Behavior = behavior;
+            this.Serializer = serializer ?? new JsonSerializer();
+        }
+
+        /// <summary>
+        /// Gets or sets the result object.
+        /// </summary>
+        /// <value>The result object.</value>
+        public T Object { get; private set; }
+        /// <summary>
+        /// Gets or sets the behavior of the action.
+        /// </summary>
+        /// <value>The behavior of the action.</value>
+        public JsonRequestBehavior Behavior { get; private set; }
+        /// <summary>
+        /// Gets or sets the serializer.
+        /// </summary>
+        /// <value>The serializer.</value>
+        public JsonSerializer Serializer { get; private set; }
+
+        /// <summary>
+        /// Enables processing of the result of an action method by a custom type that inherits from the <see cref="T:System.Web.Mvc.ActionResult"/> class.
+        /// </summary>
+        /// <param name="context">The context in which the result is executed. The context information includes the controller, HTTP content, request context, and route data.</param>
+        public override void ExecuteResult(ControllerContext context) {
+            if (Behavior == JsonRequestBehavior.DenyGet
+                && context.HttpContext.Request.HttpMethod.ToUpper() == "GET")
+                throw new NotSupportedException("JsonActionResult with Behavior=DenyGet does not support the GET method");
+
+            var response = context.HttpContext.Response;
+            response.ContentType = "application/json";
+            Serializer.Serialize(Object, response.Output);
+        }
+    }
+}
